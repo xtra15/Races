fbf1e7fa2328713ab8d2f66a69c1c4e265299aa8
unknown <mariyasalleh110@gmail.com>
2026-09-02 21:56:08 +0800

    chore(randomrace): scaffold maven project + valhallaraces compile stub

---
+++ RandomRace/src/main/java/com/yourname/randomrace/RandomRacePlugin.java
@@ -0,0 +1,10 @@
+package com.yourname.randomrace;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RandomRacePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {}
+    @Override
+    public void onDisable() {}
+}
